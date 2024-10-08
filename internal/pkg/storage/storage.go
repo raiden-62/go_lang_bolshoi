c035aa753c4226007a7968ce08df20385fd3974a
raiden-62 <afedin000@gmail.com>
2024-10-08 17:15:40 +0300

    tidied up a bit

--- internal/pkg/storage/storage.go
+++ internal/pkg/storage/storage.go
@@ -34,43 +34,44 @@ func NewStorage() (Storage, error) {
 }
 
 func (r Storage) Set(key, value string) {
-
+	defer r.logger.Sync()
 	if digit, err := strconv.Atoi(value); err == nil {
 		r.inner[key] = Variable{integer: digit, type_of_val: "D"}
-		r.logger.Info("Added value", zap.String("key", key), zap.String("value", strconv.Itoa(digit)), zap.String("type", "integer"))
-		r.logger.Sync
+		r.logger.Info("Added value", zap.String("key", key), zap.String("value", strconv.Itoa(digit)), zap.String("type", "D"))
 	} else {
 		r.inner[key] = Variable{str: value, type_of_val: "S"}
-		r.logger.Info("Added value", zap.String("key", key), zap.String("value", value), zap.String("type", "string"))
-		r.logger.Sync
+		r.logger.Info("Added value", zap.String("key", key), zap.String("value", value), zap.String("type", "S"))
 	}
 
 }
 
-func (r Storage) Get(key string) interface{} { //return pointer to the value?
+func (r Storage) Get(key string) interface{} { //return pointer to the struct?
 	value_struct, ok := r.inner[key]
+
+	defer r.logger.Sync()
 
 	if !ok {
 		return nil
 	}
 	if value_struct.type_of_val == "D" {
 		r.logger.Info("Returned value", zap.String("key", key), zap.String("value", strconv.Itoa(value_struct.integer)))
-		r.logger.Sync
 		return value_struct.integer
 	} else if value_struct.type_of_val == "S" {
 		r.logger.Info("Returned value", zap.String("key", key), zap.String("value", value_struct.str))
-		r.logger.Sync
 		return value_struct.str
+	} else {
+		return nil //should this be here?
 	}
-	return nil //should this be here?
 }
 
 func (r Storage) GetKind(key string) string {
 	value_struct, ok := r.inner[key]
 
 	r.logger.Info("Returned type of value", zap.String("type", value_struct.type_of_val))
+	r.logger.Sync()
+
 	if !ok {
-		return "somethings wrong" //this is NOT the way for sure
+		return "key not found" //this is probably not how it's done
 	}
 	return value_struct.type_of_val
 
